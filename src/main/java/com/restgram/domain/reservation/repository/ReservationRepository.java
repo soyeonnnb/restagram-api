c78860e501983e1f0e7112fcc72762094324514f
soyeonnnb <la28s5d@naver.com>
2024-05-21 22:45:22 +0900

    Feat: 예약 취소하기

--- src/main/java/com/restgram/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/restgram/domain/reservation/repository/ReservationRepository.java
@@ -1,7 +1,14 @@
 package com.restgram.domain.reservation.repository;
 
 import com.restgram.domain.reservation.entity.Reservation;
+import com.restgram.domain.reservation.entity.ReservationForm;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
+import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    @Lock(LockModeType.PESSIMISTIC_READ) // 비관적 락 사용
+    Optional<Reservation> findById(Long id);
 }
