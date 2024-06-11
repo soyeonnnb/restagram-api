a7fecaf6c77ecfc1eb7c983f2d4282b3c41426f9
soyeonnnb <la28s5d@naver.com>
2024-06-12 01:20:19 +0900

    Refactor: error 반환 형식 수정

--- src/main/java/com/restgram/domain/user/dto/request/StoreJoinRequest.java
+++ src/main/java/com/restgram/domain/user/dto/request/StoreJoinRequest.java
@@ -6,69 +6,66 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 public class StoreJoinRequest {
 
-    @Email(message = "이메일 형식이 아닙니다.")
-    @NotBlank(message = "이메일은 필수 영역입니다.")
-    @Size(min=1, max=350, message = "이메일은 최소 1자 ~ 최대 350자까지 입력 가능합니다.")
-    private String email;
+  @Email(message = "이메일 형식이 아닙니다.")
+  @NotBlank(message = "이메일은 필수 영역입니다.")
+  @Size(min = 1, max = 350, message = "이메일은 최소 1자 ~ 최대 350자까지 입력 가능합니다.")
+  private String email;
 
-    @NotBlank(message = "비밀번호는 필수 영역입니다.")
-    @Size(min=1, max=20, message = "비밀번호는 최소 1자 ~ 최대 20자까지 입력 가능합니다.")
-    private String password;
+  @NotBlank(message = "비밀번호는 필수 영역입니다.")
+  @Size(min = 1, max = 20, message = "비밀번호는 최소 1자 ~ 최대 20자까지 입력 가능합니다.")
+  private String password;
 
-    @NotBlank(message = "닉네임은 필수 영역입니다.")
-    @Size(min = 1, max = 30, message = "닉네임은 최소 1자 ~ 최대 30자까지 입력 가능합니다.")
-    private String nickname;
+  @NotBlank(message = "닉네임은 필수 영역입니다.")
+  @Size(min = 1, max = 30, message = "닉네임은 최소 1자 ~ 최대 30자까지 입력 가능합니다.")
+  private String nickname;
 
-    @NotBlank(message = "이름은 필수 영역입니다.")
-    @Size(min = 1, max = 30, message = "이름은 최소 1자 ~ 최대 30자까지 입력 가능합니다.")
-    private String name;
+  @NotBlank(message = "이름은 필수 영역입니다.")
+  @Size(min = 1, max = 30, message = "이름은 최소 1자 ~ 최대 30자까지 입력 가능합니다.")
+  private String name;
 
-    @NotBlank(message = "전화번호는 필수 영역입니다.")
-    private String phone;
+  @NotBlank(message = "전화번호는 필수 영역입니다.")
+  private String phone;
 
-    @NotBlank(message = "가게 주소는 필수 영역입니다.")
-    private String storeName;
+  @NotBlank(message = "가게 주소는 필수 영역입니다.")
+  private String storeName;
 
-    private String storePhone;
+  private String storePhone;
 
-    @NotNull(message = "행정 코드는 필수 영역입니다.")
-    private Long bcode;
+  @NotNull(message = "행정 코드는 필수 영역입니다.")
+  private Long bcode;
 
-    @NotBlank(message = "주소는 필수 영역입니다.")
-    private String address;
+  @NotBlank(message = "주소는 필수 영역입니다.")
+  private String address;
 
-    @NotBlank(message = "상세주소는 필수 영역입니다.")
-    private String detailAddress;
+  @NotBlank(message = "상세주소는 필수 영역입니다.")
+  private String detailAddress;
 
-    @NotBlank(message = "위도는 필수 영역입니다.")
-    private Double latitude;
+  @NotNull(message = "위도는 필수 영역입니다.")
+  private Double latitude;
 
-    @NotBlank(message = "경도는 필수 영역입니다.")
-    private Double longitude;
+  @NotNull(message = "경도는 필수 영역입니다.")
+  private Double longitude;
 
-    public Store of(EmdAddress emdAddress, String encodedPassword) {
-        return Store.builder()
-                .email(email)
-                .name(name)
-                .phone(phone)
-                .nickname(nickname)
-                .storeName(storeName)
-                .password(encodedPassword)
-                .latitude(latitude)
-                .longitude(longitude)
-                .emdAddress(emdAddress)
-                .address(address)
-                .detailAddress(detailAddress)
-                .storePhone(storePhone)
-                .build();
-    }
+  public Store of(EmdAddress emdAddress, String encodedPassword) {
+    return Store.builder()
+        .email(email)
+        .name(name)
+        .phone(phone)
+        .nickname(nickname)
+        .storeName(storeName)
+        .password(encodedPassword)
+        .latitude(latitude)
+        .longitude(longitude)
+        .emdAddress(emdAddress)
+        .address(address)
+        .detailAddress(detailAddress)
+        .storePhone(storePhone)
+        .build();
+  }
 
 }
--- src/main/java/com/restgram/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/restgram/global/exception/handler/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.restgram.global.exception.handler;
 
 import com.restgram.global.exception.entity.ApiResponse;
+import com.restgram.global.exception.entity.ErrorResponse;
+import com.restgram.global.exception.entity.ErrorResponse.ValidationError;
 import com.restgram.global.exception.entity.RestApiException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Path;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +19,6 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.util.List;
-import java.util.Set;
 
 // 전역적으로 에러를 처리.
 // Spring 내부에서 스프링 예외를 미리 처리해 둔 ResponseEntityExceptionHandler 존재.
@@ -26,58 +27,66 @@ import java.util.Set;
 @Slf4j
 public class GlobalExceptionHandler { // extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(RestApiException.class)
-    public ResponseEntity<ApiResponse<?>> handleRestApiException(RestApiException e, HttpServletRequest request) {
-        log.error("요청 실패 => 요청 경로: {}, 로그메세지: {}", request.getRequestURL(), e.getLog());
+  @ExceptionHandler(RestApiException.class)
+  public ResponseEntity<ApiResponse<?>> handleRestApiException(RestApiException e,
+      HttpServletRequest request) {
+    log.error("요청 실패 -> 요청 경로: {}, 로그메세지: {}", request.getRequestURL(), e.getLog());
 
-        return ResponseEntity.status(e.getErrorCode().getHttpStatus()).body(ApiResponse.createError(e.getErrorCode().getCode(), e.getErrorCode().getMessage()));
-    }
+    return ResponseEntity.status(e.getErrorCode().getHttpStatus())
+        .body(ApiResponse.createError(e.getErrorCode().getCode(), e.getErrorCode().getMessage()));
+  }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse<?>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
-        List<String> errors = fieldErrors.stream()
-                .map(fieldError -> fieldError.getField() + ", " + fieldError.getDefaultMessage())
-                .toList();
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ApiResponse<?>> handleMethodArgumentNotValidException(
+      MethodArgumentNotValidException e) {
+    List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+    List<ValidationError> errors = fieldErrors.stream()
+        .map(ErrorResponse.ValidationError::of)
+        .collect(Collectors.toList());
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.createError("ERROR-001", errors, e.getMessage()));
-    }
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(ApiResponse.createError("ERROR-001", errors, e.getMessage()));
+  }
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Object> handleIllegalArgument(IllegalArgumentException e) {
-        log.warn("handleIllegalArgument 에러 발생 Message: {}", e.getMessage());
+  @ExceptionHandler(IllegalArgumentException.class)
+  public ResponseEntity<ApiResponse<String>> handleIllegalArgument(IllegalArgumentException e) {
+    log.warn("handleIllegalArgument 에러 발생 Message: {}", e.getMessage());
 
-        // 사용자에게 전달할 에러 메시지를 설정
-        String errorMessage = "적절하지 않은 인자를 메소드에 넘겨주었습니다. 파라미터를 확인해주세요.";
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.createError("ERROR-002", errorMessage, e.getMessage()));
-    }
-
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<ApiResponse<?>> handleConstraintViolationException(ConstraintViolationException e) {
-        Set<ConstraintViolation<?>> constraintViolations = e.getConstraintViolations();
-
-        List<String> errors = constraintViolations.stream()
-                .map(constraintViolation -> extractField(constraintViolation.getPropertyPath()) + ", " + constraintViolation.getMessage())
-                .toList();
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.createError("ERROR-003", errors, e.getMessage()));
-    }
-
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<ApiResponse<?>> handleHttpMessageNotReadableException(HttpMessageNotReadableException e, HttpServletRequest request) {
-        log.error("요청 실패 => 요청 경로: {}, 오류메세지: {}", request.getRequestURL(), e.getMessage());
-
-        // 사용자에게 전달할 에러 메시지를 설정
-        String errorMessage = "잘못된 요청 본문 형식입니다. 입력 데이터를 확인해주세요.";
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.createError("ERROR-004", errorMessage, e.getMessage()));
-    }
-
-    private String extractField(Path path){
-        String[] arrays = path.toString().split("[.]");
-        int index = arrays.length - 1;
-        return arrays[index];
-    }
+    // 사용자에게 전달할 에러 메시지를 설정
+    String errorMessage = "적절하지 않은 인자를 메소드에 넘겨주었습니다. 파라미터를 확인해주세요.";
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(ApiResponse.createError("ERROR-002", errorMessage, e.getMessage()));
+  }
 
 
+  @ExceptionHandler(ConstraintViolationException.class)
+  public ResponseEntity<ApiResponse<?>> handleConstraintViolationException(
+      ConstraintViolationException e) {
+    Set<ConstraintViolation<?>> constraintViolations = e.getConstraintViolations();
+
+    List<String> errors = constraintViolations.stream()
+        .map(constraintViolation -> extractField(constraintViolation.getPropertyPath()) + ", "
+            + constraintViolation.getMessage())
+        .toList();
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(ApiResponse.createError("ERROR-003", errors, e.getMessage()));
+  }
+
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public ResponseEntity<ApiResponse<?>> handleHttpMessageNotReadableException(
+      HttpMessageNotReadableException e, HttpServletRequest request) {
+    log.error("요청 실패 => 요청 경로: {}, 오류메세지: {}", request.getRequestURL(), e.getMessage());
+
+    // 사용자에게 전달할 에러 메시지를 설정
+    String errorMessage = "잘못된 요청 본문 형식입니다. 입력 데이터를 확인해주세요.";
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(ApiResponse.createError("ERROR-004", errorMessage, e.getMessage()));
+  }
+
+  private String extractField(Path path) {
+    String[] arrays = path.toString().split("[.]");
+    int index = arrays.length - 1;
+    return arrays[index];
+  }
 }
