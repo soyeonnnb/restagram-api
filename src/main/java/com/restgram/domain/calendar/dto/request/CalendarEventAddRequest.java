5926319beaf7582ede68594ec8f3b9cfcb0179c1
soyeonnnb <la28s5d@naver.com>
2024-06-11 20:05:43 +0900

    Refactor: calendar domain 엔티티 제약조건 추가

--- src/main/java/com/restgram/domain/calendar/dto/request/CalendarEventAddRequest.java
+++ src/main/java/com/restgram/domain/calendar/dto/request/CalendarEventAddRequest.java
@@ -1,5 +1,7 @@
 package com.restgram.domain.calendar.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Builder
@@ -7,6 +9,9 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class CalendarEventAddRequest {
+    @NotBlank(message = "캘린더 ID는 필수 영역입니다.")
     private String calendar_id;
+
+    @NotBlank(message = "일정 Event는 필수 영역입니다.")
     private EventCreate event;
 }
