e4191aeb470f38f234772ae5c539d609dbde6753
soyeonnnb <la28s5d@naver.com>
2024-06-12 00:12:45 +0900

    Refactor: reservation 도메인 에러 로그 추가 및 stream 변경

--- src/main/java/com/restgram/domain/reservation/service/impl/ReservationFormServiceImpl.java
+++ src/main/java/com/restgram/domain/reservation/service/impl/ReservationFormServiceImpl.java
@@ -13,94 +13,115 @@ import com.restgram.domain.user.repository.StoreRepository;
 import com.restgram.global.exception.entity.RestApiException;
 import com.restgram.global.exception.errorCode.ReservationErrorCode;
 import com.restgram.global.exception.errorCode.UserErrorCode;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ReservationFormServiceImpl implements ReservationFormService {
 
-    private final StoreRepository storeRepository;
-    private final ReservationFormRepository reservationFormRepository;
+  private final StoreRepository storeRepository;
+  private final ReservationFormRepository reservationFormRepository;
 
-    @Override
-    @Transactional
-    public void addReservationForm(Long userId, ReservationFormRequest request) {
-        Store store = storeRepository.findById(userId).orElseThrow(() -> new RestApiException(UserErrorCode.INVALID_USER_ID));
+  @Override
+  @Transactional
+  public void addReservationForm(Long userId, ReservationFormRequest request) {
+    Store store = storeRepository.findById(userId)
+        .orElseThrow(() -> new RestApiException(UserErrorCode.INVALID_LOGIN_USER_ID,
+            "[가게] 로그인 사용자ID가 유효하지 않습니다. [로그인 사용자ID=" + userId + "]"));
 
-        // 하루씩 키워나가면서
-        for(LocalDate date = request.getStartAt(); date.isBefore(request.getFinishAt().plusDays(1)); date = date.plusDays(1)) {
-            // 만약 제외 날짜에 포함되어 있다면 패스
-            if (request.getExceptDateList().contains(date)) continue;
+    // 하루씩 키워나가면서
+    for (LocalDate date = request.getStartAt(); date.isBefore(request.getFinishAt().plusDays(1));
+        date = date.plusDays(1)) {
+      // 만약 제외 날짜에 포함되어 있다면 패스
+      if (request.getExceptDateList().contains(date)) {
+        continue;
+      }
 
-            for(ReservationFormDateRequest datetable : request.getWeekListMap().getOrDefault(date.getDayOfWeek(), new ArrayList<>())) {
-                // 만약 해당 날짜 + 시간에 이미 예약폼이 존재한다면 테이블 수 늘리기
-                Optional<ReservationForm> optionalReservationForm = reservationFormRepository.findByStoreAndDateAndTime(store, date, datetable.getTime());
-                if (optionalReservationForm.isPresent()) {
-                    optionalReservationForm.get().updateRemainQuantity(datetable.getTable());
-                } else {
-                    ReservationForm form = ReservationForm.builder()
-                            .store(store)
-                            .date(date)
-                            .time(datetable.getTime())
-                            .quantity(datetable.getTable())
-                            .remainQuantity(datetable.getTable())
-                            .tablePerson(request.getTablePerson())
-                            .maxReservationPerson(request.getMaxReservationPerson())
-                            .state(ReservationFormState.ACTIVE)
-                            .build();
-                    reservationFormRepository.save(form);
-
-                }
-            }
+      for (ReservationFormDateRequest datetable : request.getWeekListMap()
+          .getOrDefault(date.getDayOfWeek(), new ArrayList<>())) {
+        // 만약 해당 날짜 + 시간에 이미 예약폼이 존재한다면 테이블 수 늘리기
+        Optional<ReservationForm> optionalReservationForm = reservationFormRepository.findByStoreAndDateAndTime(
+            store, date, datetable.getTime());
+        if (optionalReservationForm.isPresent()) {
+          optionalReservationForm.get().updateRemainQuantity(datetable.getTable());
+        } else {
+          ReservationForm form = ReservationForm.builder().store(store).date(date)
+              .time(datetable.getTime()).quantity(datetable.getTable())
+              .remainQuantity(datetable.getTable()).tablePerson(request.getTablePerson())
+              .maxReservationPerson(request.getMaxReservationPerson())
+              .state(ReservationFormState.ACTIVE).build();
+          reservationFormRepository.save(form);
         }
+      }
+    }
+  }
+
+  @Override
+  @Transactional(readOnly = true)
+  public List<ReservationFormResponse> getReservationForm(Long storeId, Integer year,
+      Integer month) {
+    Store store = storeRepository.findById(storeId)
+        .orElseThrow(() -> new RestApiException(UserErrorCode.INVALID_LOGIN_USER_ID,
+            "[가게] 로그인 사용자ID가 유효하지 않습니다. [로그인 사용자ID=" + storeId + "]"));
+    // 해당 달
+    LocalDate date = LocalDate.of(year, month, 1);
+
+    List<ReservationForm> reservationFormList = reservationFormRepository.findAllByStoreAndDateBetweenAndStateEquals(
+        store, date, date.plusMonths(1).minusDays(1), ReservationFormState.ACTIVE);
+
+    return reservationFormList.stream().map(ReservationFormResponse::of)
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  @Transactional(readOnly = true)
+  public List<ReservationFormResponse> getStoreReservationForm(Long storeId, Integer year,
+      Integer month) {
+    Store store = storeRepository.findById(storeId)
+        .orElseThrow(() -> new RestApiException(UserErrorCode.INVALID_LOGIN_USER_ID,
+            "[가게] 로그인 사용자ID가 유효하지 않습니다. [로그인 사용자ID=" + storeId + "]"));
+    // 해당 달
+    LocalDate date = LocalDate.of(year, month, 1);
+
+    List<ReservationForm> reservationFormList = reservationFormRepository.findAllByStoreAndDateBetween(
+        store, date, date.plusMonths(1).minusDays(1));
+
+    return reservationFormList.stream().map(ReservationFormResponse::of)
+        .collect(Collectors.toList());
+  }
+
+  // 예약 폼 변경
+  @Override
+  @Transactional
+  public void updateReservationState(Long storeId, UpdateReservationFormStateRequest request) {
+    ReservationForm form = reservationFormRepository.findById(request.getId())
+        .orElseThrow(() -> new RestApiException(ReservationErrorCode.INVALID_RESERVATION_FORM_ID,
+            "예약폼ID가 유효하지 않습니다. [예약폼ID=" + request.getId() + "]"));
+
+    // 유저 일치 확인
+    if (form.getStore().getId() != storeId) {
+      throw new RestApiException(UserErrorCode.USER_MISMATCH,
+          "로그인 사용자와 예약폼 작성 가게 사용자ID가 일치하지 않습니다. [로그인 사용자ID=" + storeId + ", 예약폼 작성 가게 사용자ID="
+              + form.getStore().getId() + "]");
+    }
+    LocalDateTime formDateTime = LocalDateTime.of(form.getDate(), form.getTime());
+
+    // 이미 시간이 지나간 예약폼은 수정 불가
+    if (formDateTime.isBefore(LocalDateTime.now())) {
+      throw new RestApiException(ReservationErrorCode.RESERVATION_IS_BEFORE_NOW,
+          "종료된 예약폼은 수정이 불가합니다. [예약폼ID=" + request.getId() + ", 예약폼 날짜=" + form.getDate() + ", 시간="
+              + form.getTime() + "]");
     }
 
-    @Override
-    @Transactional(readOnly = true)
-    public List<ReservationFormResponse> getReservationForm(Long storeId, Integer year, Integer month) {
-        Store store = storeRepository.findById(storeId).orElseThrow(() -> new RestApiException(UserErrorCode.INVALID_USER_ID));
-        LocalDate date = LocalDate.of(year, month, 1);
-        List<ReservationForm> reservationFormList = reservationFormRepository.findAllByStoreAndDateBetweenAndStateEquals(store, date, date.plusMonths(1).minusDays(1), ReservationFormState.ACTIVE);
-        List<ReservationFormResponse> customerReservationFormResponseList = new ArrayList<>();
-        for(ReservationForm form : reservationFormList) {
-            customerReservationFormResponseList.add(ReservationFormResponse.of(form));
-        }
-        return customerReservationFormResponseList;
-    }
-
-    @Override
-    @Transactional(readOnly = true)
-    public List<ReservationFormResponse> getStoreReservationForm(Long storeId, Integer year, Integer month) {
-        Store store = storeRepository.findById(storeId).orElseThrow(() -> new RestApiException(UserErrorCode.INVALID_USER_ID));
-        LocalDate date = LocalDate.of(year, month, 1);
-        List<ReservationForm> reservationFormList = reservationFormRepository.findAllByStoreAndDateBetween(store, date, date.plusMonths(1).minusDays(1));
-        List<ReservationFormResponse> storeReservationFormResponseList = new ArrayList<>();
-        for(ReservationForm form : reservationFormList) {
-            storeReservationFormResponseList.add(ReservationFormResponse.of(form));
-        }
-        return storeReservationFormResponseList;
-    }
-
-    // 예약 폼 변경
-    @Override
-    @Transactional
-    public void updateReservationState(Long storeId, UpdateReservationFormStateRequest request) {
-        ReservationForm form = reservationFormRepository.findById(request.getId()).orElseThrow(() -> new RestApiException(ReservationErrorCode.INVALID_RESERVATION_FORM_ID));
-        // 유저 일치 확인
-        if (form.getStore().getId() != storeId) throw new RestApiException(UserErrorCode.USER_MISMATCH);
-        LocalDateTime formDateTime = LocalDateTime.of(form.getDate(), form .getTime());
-        // 이미 시간이 지나간 예약폼은 수정 불가
-        if (formDateTime.isBefore(LocalDateTime.now())) throw new RestApiException(ReservationErrorCode.RESERVATION_IS_BEFORE_NOW);
-        form.updateState(request.getState());
-        reservationFormRepository.save(form);
-    }
+    form.updateState(request.getState());
+  }
 
 }
--- src/main/java/com/restgram/domain/reservation/service/impl/ReservationServiceImpl.java
+++ src/main/java/com/restgram/domain/reservation/service/impl/ReservationServiceImpl.java
@@ -6,14 +6,18 @@ import com.restgram.domain.reservation.dto.request.AddReservationRequest;
 import com.restgram.domain.reservation.dto.request.DeleteReservationRequest;
 import com.restgram.domain.reservation.dto.response.CustomerReservationResponse;
 import com.restgram.domain.reservation.dto.response.StoreReservationResponse;
-import com.restgram.domain.reservation.entity.*;
+import com.restgram.domain.reservation.entity.Reservation;
+import com.restgram.domain.reservation.entity.ReservationCancel;
+import com.restgram.domain.reservation.entity.ReservationCancelState;
+import com.restgram.domain.reservation.entity.ReservationForm;
+import com.restgram.domain.reservation.entity.ReservationFormState;
+import com.restgram.domain.reservation.entity.ReservationState;
 import com.restgram.domain.reservation.repository.ReservationCancelRepository;
 import com.restgram.domain.reservation.repository.ReservationFormRepository;
 import com.restgram.domain.reservation.repository.ReservationRepository;
 import com.restgram.domain.reservation.service.ReservationService;
 import com.restgram.domain.user.entity.Customer;
 import com.restgram.domain.user.entity.Store;
-import com.restgram.domain.user.entity.User;
 import com.restgram.domain.user.repository.CustomerRepository;
 import com.restgram.domain.user.repository.StoreRepository;
 import com.restgram.domain.user.repository.UserRepository;
@@ -22,137 +26,194 @@ import com.restgram.global.exception.errorCode.ReservationErrorCode;
 import com.restgram.global.exception.errorCode.UserErrorCode;
 import com.restgram.global.sse.entity.NotificationType;
 import com.restgram.global.sse.service.NotificationService;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ReservationServiceImpl implements ReservationService {
 
-    private final UserRepository userRepository;
-    private final CustomerRepository customerRepository;
-    private final ReservationFormRepository reservationFormRepository;
-    private final ReservationRepository reservationRepository;
-    private final ReservationCancelRepository reservationCancelRepository;
-    private final StoreRepository storeRepository;
-    private final NotificationService notificationService;
-    private final CalendarEventService calendarEventService;
-    private final CalendarEventRepository calendarEventRepository;
+  private final UserRepository userRepository;
+  private final CustomerRepository customerRepository;
+  private final ReservationFormRepository reservationFormRepository;
+  private final ReservationRepository reservationRepository;
+  private final ReservationCancelRepository reservationCancelRepository;
+  private final StoreRepository storeRepository;
+  private final NotificationService notificationService;
+  private final CalendarEventService calendarEventService;
+  private final CalendarEventRepository calendarEventRepository;
 
-    @Override
-    @Transactional
-    public void addReservation(Long userId, AddReservationRequest request) {
-        Customer customer = customerRepository.findById(userId).orElseThrow(() -> new RestApiException(UserErrorCode.INVALID_USER_ID));
-        ReservationForm form = reservationFormRepository.findById(request.getReservationFormId()).orElseThrow(() -> new RestApiException(ReservationErrorCode.INVALID_RESERVATION_FORM_ID));
-        // 예약 일자 확인
-        LocalDateTime formDate = LocalDateTime.of(form.getDate(), form.getTime());
-        if (formDate.isBefore(LocalDateTime.now())) throw new RestApiException(ReservationErrorCode.RESERVATION_IS_BEFORE_NOW);
-        // 테이블이 가능한지 확인
-        if (form.getState().toString().equals(ReservationFormState.DISABLE)) throw new RestApiException(ReservationErrorCode.RESERVATION_DISABLE);
-        // 테이블 수 확인
-        int tableNum = getTableNum(request.getHeadCount(), form.getTablePerson());
-        if (form.getRemainQuantity() < tableNum) {
-            throw new RestApiException(ReservationErrorCode.TABLE_UNAVAILABLE);
-        }
-        // 예약 가능 수 확인
-        if (form.getMaxReservationPerson() < request.getHeadCount()) throw new RestApiException(ReservationErrorCode.RESERVATION_HEAD_COUNT_EXCEED_TABLE_NUM);
+  @Override
+  @Transactional
+  public void addReservation(Long userId, AddReservationRequest request) {
+    Customer customer = customerRepository.findById(userId)
+        .orElseThrow(() -> new RestApiException(UserErrorCode.INVALID_LOGIN_USER_ID,
+            "[일반] 로그인 사용자ID가 유효하지 않습니다. [로그인 사용자ID=" + userId + "]"));
 
-        // 예약 생성
-        Reservation reservation = request.of(form, form.getStore(), customer);
-        reservationRepository.save(reservation);
-        form.updateRemainQuantity(tableNum * -1);
+    ReservationForm form = reservationFormRepository.findById(request.getReservationFormId())
+        .orElseThrow(() -> new RestApiException(ReservationErrorCode.INVALID_RESERVATION_FORM_ID,
+            "예약폼ID가 유효하지 않습니다. [예약폼ID=" + request.getReservationFormId() + "]"));
 
-        // 예약 생성 알림 보내기
-        notificationService.send(reservation.getStore(), NotificationType.NEW_RESERVATION, reservation);
+    // 예약 일자 확인
+    LocalDateTime formDate = LocalDateTime.of(form.getDate(), form.getTime());
 
-        // 만약 캘린더 동의가 되어 있다면 일정에 추가하기
-        if (customer.isCalendarAgree()) {
-            calendarEventService.createCalendarEvent(reservation);
-        }
-
+    if (formDate.isBefore(LocalDateTime.now())) {
+      throw new RestApiException(ReservationErrorCode.RESERVATION_IS_BEFORE_NOW,
+          "이전 날짜는 예약할 수 없습니다. [예약폼ID=" + request.getReservationFormId() + ", 예약폼 날짜=" + formDate
+              + "]");
+    }
+    if (form.getState().toString().equals(ReservationFormState.DISABLE)) {
+      throw new RestApiException(ReservationErrorCode.RESERVATION_DISABLE,
+          "예약이 불가능한 상태입니다. [예약폼ID=" + request.getReservationFormId() + "]");
+    }
+    // 테이블 수 확인
+    int tableNum = getTableNum(request.getHeadCount(), form.getTablePerson());
+    if (form.getRemainQuantity() < tableNum) {
+      throw new RestApiException(ReservationErrorCode.TABLE_UNAVAILABLE,
+          "테이블 수가 부족합니다. [예약폼ID=" + request.getReservationFormId() + ", 남은 테이블 수="
+              + form.getRemainQuantity() + ", 요청 테이블 수=" + tableNum + "]");
+    }
+    // 예약 가능 수 확인
+    if (form.getMaxReservationPerson() < request.getHeadCount()) {
+      throw new RestApiException(ReservationErrorCode.RESERVATION_HEAD_COUNT_EXCEED_TABLE_NUM,
+          "최대 예약 인원수를 초과하였습니다. [예약폼ID=" + request.getReservationFormId() + ", 최대 예약 인원수="
+              + form.getMaxReservationPerson() + ", 요청 예약 인원수=" + request.getHeadCount() + "]");
     }
 
-    @Override
-    @Transactional
-    public void cancelReservation(Long userId, DeleteReservationRequest request) {
-        User user = userRepository.findById(userId).orElseThrow(() -> new RestApiException(UserErrorCode.INVALID_USER_ID));
-        Reservation reservation = reservationRepository.findById(request.getReservationId()).orElseThrow(() -> new RestApiException(ReservationErrorCode.INVALID_RESERVATION_ID));
-        // 만약 해당 유저와 관계 없는 예약이면 삭제 취소 불가
-        if ((request.getState().equals(ReservationCancelState.CUSTOMER) && reservation.getCustomer().getId() != userId)
-            || (request.getState().equals(ReservationCancelState.STORE) && reservation.getStore().getId() != userId))
-            throw new RestApiException(UserErrorCode.USER_MISMATCH);
+    // 예약 생성
+    Reservation reservation = request.of(form, form.getStore(), customer);
+    reservationRepository.save(reservation);
+    form.updateRemainQuantity(tableNum * -1);
 
-        // 이미 취소처리되었거나 기간이 지났으면 예약이면 취소 불가
-        if (!reservation.getState().equals(ReservationState.ACTIVE) || reservation.getDatetime().isBefore(LocalDateTime.now())) throw new RestApiException(ReservationErrorCode.RESERVATION_CANCEL_DISABLE);
+    // 예약 생성 알림 보내기
+    notificationService.send(reservation.getStore(), NotificationType.NEW_RESERVATION, reservation);
 
-        ReservationCancel reservationCancel = request.of(reservation);
-
-        // 삭제 처리
-        if (request.getState().equals(ReservationCancelState.CUSTOMER)) reservation.updateState(ReservationState.USER_CANCELED);
-        else if (request.getState().equals(ReservationCancelState.STORE)) reservation.updateState(ReservationState.STORE_CANCELED);
-
-        // 테이블 수 업데이트
-        reservation.getReservationForm().updateRemainQuantity(getTableNum(reservation.getHeadCount(), reservation.getReservationForm().getTablePerson()));
-
-        // 엔티티 수정& 저장
-        reservationCancelRepository.save(reservationCancel);
-
-        // 알림 보내기
-        if (request.getState().equals(ReservationCancelState.CUSTOMER)) {
-            notificationService.send(reservation.getStore(), NotificationType.CUSTOMER_RESERVATION_CANCEL, reservation);
-        } else {
-            notificationService.send(reservation.getCustomer(), NotificationType.STORE_RESERVATION_CANCEL, reservation);
-        }
-
-        // 카카오 일정 삭제
-        if (calendarEventRepository.existsByReservation(reservation)) {
-            calendarEventService.deleteCalendarEvent(reservation);
-        }
+    // 만약 캘린더 동의가 되어 있다면 일정에 추가하기
+    if (customer.isCalendarAgree()) {
+      calendarEventService.createCalendarEvent(reservation);
     }
 
-    // 구매자 예약 리스트
-    @Override
-    public List<CustomerReservationResponse> getCustomerReservations(Long userId) {
-        Customer customer = customerRepository.findById(userId).orElseThrow(() -> new RestApiException(UserErrorCode.INVALID_USER_ID));
-        List<Reservation> reservationList = reservationRepository.findAllByCustomerOrderByDatetimeDesc(customer);
-        List<CustomerReservationResponse> customerReservationResponseList = new ArrayList<>();
-        for(Reservation reservation : reservationList) {
-            if (reservation.getState().equals(ReservationState.ACTIVE)) {
-                customerReservationResponseList.add(CustomerReservationResponse.of(reservation));
-            } else {
-                ReservationCancel reservationCancel = reservationCancelRepository.findByReservation(reservation).orElseThrow(() -> new RestApiException(ReservationErrorCode.INVALID_RESERVATION_CANCEL_ID));
-                customerReservationResponseList.add(CustomerReservationResponse.of(reservation, reservationCancel.getMemo()));
-            }
-        }
-        return customerReservationResponseList;
+  }
+
+  @Override
+  @Transactional
+  public void cancelReservation(Long userId, DeleteReservationRequest request) {
+    Reservation reservation = reservationRepository.findById(request.getReservationId())
+        .orElseThrow(() -> new RestApiException(ReservationErrorCode.INVALID_RESERVATION_ID,
+            "예약ID가 유효하지 않습니다. [예약ID=" + request.getReservationId() + "]"));
+    // 만약 해당 유저와 관계 없는 예약이면 삭제 취소 불가
+    if ((request.getState().equals(ReservationCancelState.CUSTOMER)
+        && reservation.getCustomer().getId() != userId) || (
+        request.getState().equals(ReservationCancelState.STORE)
+            && reservation.getStore().getId() != userId)) {
+      throw new RestApiException(UserErrorCode.USER_MISMATCH,
+          "로그인 사용자와 관계 없는 예약입니다. [로그인 사용자ID=" + userId + ", 예약ID=" + request.getReservationId()
+              + "]");
     }
 
-    @Override
-    public List<StoreReservationResponse> getStoreReservations(Long userId, Integer year, Integer month) {
-        Store store = storeRepository.findById(userId).orElseThrow(() -> new RestApiException(UserErrorCode.INVALID_USER_ID));
-        LocalDateTime startAt = LocalDateTime.of(year, month, 1, 0, 0);
-        // 해당 가게의, 연도/월 검색
-        List<Reservation> reservationList = reservationRepository.findAllByStoreAndDatetimeGreaterThanEqualAndDatetimeLessThanOrderByDatetime(store, startAt, startAt.plusMonths(1));
-        List<StoreReservationResponse> storeReservationResponseList = new ArrayList<>();
-        for(Reservation reservation : reservationList) {
-            if (reservation.getState().equals(ReservationState.ACTIVE)) {
-                storeReservationResponseList.add(StoreReservationResponse.of(reservation));
-            } else {
-                ReservationCancel reservationCancel = reservationCancelRepository.findByReservation(reservation).orElseThrow(() -> new RestApiException(ReservationErrorCode.INVALID_RESERVATION_CANCEL_ID));
-                storeReservationResponseList.add(StoreReservationResponse.of(reservation, reservationCancel.getMemo()));
-            }
-        }
-        return storeReservationResponseList;
+    // 이미 취소처리되었거나 기간이 지났으면 예약이면 취소 불가
+    if (!reservation.getState().equals(ReservationState.ACTIVE) || reservation.getDatetime()
+        .isBefore(LocalDateTime.now())) {
+      throw new RestApiException(ReservationErrorCode.RESERVATION_CANCEL_DISABLE,
+          "취소가능한 상태가 아닙니다. [예약ID=" + request.getReservationId() + "]");
     }
 
-    private int getTableNum(Integer headcount, Integer tablePerson) {
-        Integer table = headcount / tablePerson;
-        if (headcount%tablePerson != 0) table++;
-        return table;
+    ReservationCancel reservationCancel = request.of(reservation);
+
+    // 삭제 처리
+    if (request.getState().equals(ReservationCancelState.CUSTOMER)) {
+      reservation.updateState(ReservationState.USER_CANCELED);
+    } else if (request.getState().equals(ReservationCancelState.STORE)) {
+      reservation.updateState(ReservationState.STORE_CANCELED);
     }
+
+    // 테이블 수 업데이트
+    reservation.getReservationForm().updateRemainQuantity(
+        getTableNum(reservation.getHeadCount(), reservation.getReservationForm().getTablePerson()));
+
+    // 엔티티 저장
+    reservationCancelRepository.save(reservationCancel);
+
+    // 알림 보내기
+    if (request.getState().equals(ReservationCancelState.CUSTOMER)) {
+      notificationService.send(reservation.getStore(), NotificationType.CUSTOMER_RESERVATION_CANCEL,
+          reservation);
+    } else {
+      notificationService.send(reservation.getCustomer(), NotificationType.STORE_RESERVATION_CANCEL,
+          reservation);
+    }
+
+    // 카카오 일정 삭제
+    if (calendarEventRepository.existsByReservation(reservation)) {
+      calendarEventService.deleteCalendarEvent(reservation);
+    }
+  }
+
+  // 구매자 예약 리스트
+  @Override
+  @Transactional(readOnly = true)
+  public List<CustomerReservationResponse> getCustomerReservations(Long userId) {
+    Customer customer = customerRepository.findById(userId)
+        .orElseThrow(() -> new RestApiException(UserErrorCode.INVALID_LOGIN_USER_ID,
+            "[일반] 로그인 사용자ID가 유효하지 않습니다. [로그인 사용자ID=" + userId + "]"));
+
+    List<Reservation> reservationList = reservationRepository.findAllByCustomerOrderByDatetimeDesc(
+        customer);
+
+    return reservationList.stream()
+        .map(reservation -> {
+          if (reservation.getState().equals(ReservationState.ACTIVE)) { // 예약이 정상 상태
+            return CustomerReservationResponse.of(reservation);
+          } else { // 예약이 취소상태일 시 취소 메세지도 함께보냄
+            ReservationCancel reservationCancel = reservationCancelRepository.findByReservation(
+                    reservation)
+                .orElseThrow(
+                    () -> new RestApiException(ReservationErrorCode.CANNOT_FIND_RESERVATION_CANCEL,
+                        "예약 취소 데이터를 찾을 수 없습니다. [예약ID=" + reservation.getId() + "]"));
+            return CustomerReservationResponse.of(reservation, reservationCancel.getMemo());
+          }
+        })
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  @Transactional(readOnly = true)
+  public List<StoreReservationResponse> getStoreReservations(Long userId, Integer year,
+      Integer month) {
+    Store store = storeRepository.findById(userId)
+        .orElseThrow(() -> new RestApiException(UserErrorCode.INVALID_LOGIN_USER_ID,
+            "[가게] 로그인 사용자ID가 유효하지 않습니다. [로그인 사용자ID=" + userId + "]"));
+
+    LocalDateTime startAt = LocalDateTime.of(year, month, 1, 0, 0);
+    // 해당 가게의, 연도/월 검색
+    List<Reservation> reservationList = reservationRepository.findAllByStoreAndDatetimeGreaterThanEqualAndDatetimeLessThanOrderByDatetime(
+        store, startAt, startAt.plusMonths(1));
+
+    return reservationList.stream()
+        .map(reservation -> {
+          if (reservation.getState().equals(ReservationState.ACTIVE)) {// 예약이 정상 상태
+            return StoreReservationResponse.of(reservation);
+          } else { // 예약이 취소상태일 시 취소 메세지도 함께보냄
+            ReservationCancel reservationCancel = reservationCancelRepository.findByReservation(
+                    reservation)
+                .orElseThrow(
+                    () -> new RestApiException(ReservationErrorCode.CANNOT_FIND_RESERVATION_CANCEL,
+                        "예약 취소 데이터를 찾을 수 없습니다. [예약ID=" + reservation.getId() + "]"));
+            return StoreReservationResponse.of(reservation, reservationCancel.getMemo());
+          }
+        })
+        .collect(Collectors.toList());
+  }
+
+  private int getTableNum(Integer headcount, Integer tablePerson) {
+    Integer table = headcount / tablePerson;
+    if (headcount % tablePerson != 0) {
+      table++;
+    }
+    return table;
+  }
 }
